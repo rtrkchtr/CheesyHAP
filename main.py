7163106e242711755384186d547a2bb08af168bb
rtrkchtr <kechterartur@gmail.com>
2020-04-11 12:23:34 +0200

    First Version CheesyHAP

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 
 import sys, os
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QListWidgetItem, QPushButton, QLabel
-from PyQt5.QtCore import Qt, QUrl
+from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QListWidgetItem, QTextEdit
+from PyQt5.QtCore import Qt, QUrl, QRunnable, pyqtSlot, QThreadPool, pyqtSignal, QObject
 from PyQt5.QtGui import QPixmap
 from ui.mainwindow import Ui_MainWindow
 import subprocess, os
@@ -52,6 +52,14 @@ class ListBox(QListWidget):
         return self.filepaths
 
 
+class modQTestEdit(QTextEdit):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setAcceptDrops(True)
+        self.setGeometry(10,40,271,31)
+
+
+
 # ----------------------- inherited modified Main Window Class -------------------------
 
 class modifiedMainWindow(Ui_MainWindow):
@@ -68,27 +76,37 @@ class Mainframe(QMainWindow):
         self.ui.bx = ListBox(self.ui.filelist)
         pixmap = QPixmap('ressources/kaese2.png')
         self.ui.pixmap_label_7.setPixmap(pixmap)
+        self.ui.set_output_directory = modQTestEdit(self.ui.Output_dir_textEdit)
 
     def get_number_of_urls(self, urls):
-        kazoo = len(urls)
-        print("total number of urls: " + str(kazoo))
-        return kazoo
+        url_len = len(urls) # Renamed variable from 'kazoo' to a more meaningful name ;)
+        print("total number of urls: " + str(url_len))
+        return url_len
 
     def getSelectedItem(self):
         item = QListWidgetItem(self.listView.currentItem())
         return item.text()
 
-    def set_outputdir(self):
-        path = 'C:/Users/kecht/Desktop/output_test/'
-        return path
 
     def get_output_directory(self):
         outputfilepath = self.ui.Output_dir_textEdit.toPlainText()
+        print(outputfilepath)
+        # if 'file:///' in outputfilepath:
+        #     index_remove = outputfilepath.find('file:///')
+        #     outputfilepath = [index_remove+len('file:///'):]
+        outputfilepath = outputfilepath.replace('file:///', '')
+        print(outputfilepath)
+
         if os.path.isdir(outputfilepath):
             return outputfilepath
-            print(outputfilepath)
         else:
             print("this box should contain only directories. This not a directory!")
+
+        # Renamed this from 'set_ouputdir' to 'output_dir' since it doesn't change any variables.
+    def output_dir(self):
+        # Changed this to be OS and user independent :)
+        # return os.path.join(os.path.expanduser("~/Desktop/"+str(self.get_output_directory())))
+        return os.path.join(os.path.expanduser(str(self.get_output_directory()+"/")))
 
     def get_filepaths(self):
         files = self.ui.bx.get_filepaths()
@@ -100,6 +118,7 @@ class Mainframe(QMainWindow):
                 return(self.checkPaths(files))
 
     # check if paths are files or directories:
+
     def checkPaths(self, filepaths):
         total_filepaths = []
         for file in filepaths:
@@ -115,23 +134,21 @@ class Mainframe(QMainWindow):
 
     # This method takes the radiobutton input from the ui and returns Users input
     def get_format_to_convert_to(self):
-        selection = None
+        # I would simplifed this to prevent accidental fall-though behavior with ifs.
         if self.ui.radioButton_Hap.isChecked():
-            selection = "hap"
             print("hap format selected")
+            return "hap"
         if self.ui.radioButton_HapAlpha.isChecked():
-            selection = "hap -format hap_alpha"
+            return "hap -format hap_alpha"
         if self.ui.radioButton_HapQ.isChecked():
-            selection = "hap -format hap_q"
+            return "hap -format hap_q"
         if self.ui.radioButton_Mjpeg.isChecked():
-            selection = "mjpeg -q:v 3"
-        if selection == None:
-            print("pleasy specify format")
-        print(format)
-        return selection
+            return "mjpeg -q:v 3"
+        print("please specify format")
 
     def get_resolutions(self):
-        state = self.ui.orig_resolution_checkBox.isChecked()
+        # I don't think 'state' is used anywhere else, so it can be removed. :)
+        # state = self.ui.orig_resolution_checkBox.isChecked()
         if self.ui.orig_resolution_checkBox.isChecked():
             resolution = None
         else:
@@ -142,19 +159,23 @@ class Mainframe(QMainWindow):
     def check_parameter(self):
         pass
 
-    def get_strings_to_remove(self):
-        toErase = self.ui.removeInput.toPlainText()
-        eraselist = toErase.split(";")
-        print(eraselist)
-        return eraselist
+    # @property
+    # def strings_to_remove(self):
+    #     toErase = self.ui.removeInput.toPlainText()
+    #     eraselist = toErase.split(";")
+    #     index = 0
+    #     for element in eraselist:
+    #         if ' ' in eraselist[index]:
+    #             eraselist[index] = element.replace(' ', '')
+    #         index += 1
+    #     print(eraselist)
+    #     return eraselist
 
     def get_prefix(self):
         prefix = self.ui.prefixInput.toPlainText()
         if prefix == "":
             return None
-        else:
-            prefix = self.ui.prefixInput.toPlainText()
-        return prefix
+        return self.ui.prefixInput.toPlainText() # Just shortened these lines, no else necessary since the 'if' returns before
 
 
     def set_statusbar(self, maxValue, index):
@@ -166,10 +187,9 @@ class Mainframe(QMainWindow):
         # this method will contain tests in the future
         # this is a littlebit redundant.....
         paths = self.get_filepaths()
-        #outdir = self.get_output_directory()
-        outdir = self.set_outputdir()
+        outdir = self.output_dir()
         format = self.get_format_to_convert_to()
-        to_erase = self.get_strings_to_remove()
+        to_erase = self.strings_to_remove
         resolution = self.get_resolutions()
         prefix = self.get_prefix()
 
@@ -177,13 +197,71 @@ class Mainframe(QMainWindow):
         # Here we call the method
         self.run(paths, outdir, format, resolution, to_erase, prefix)
 
-    def run(self, pathlist, outdirectory, format, resolution, eraselist, prefix):
-        for path in pathlist:
-            ffmpeg_object = ffmpeg_converter(path, outdirectory, format, resolution, eraselist, prefix) # creates a object
-            cmd = ffmpeg_object.command_concat()
-            ffmpeg_object.subprocess_run(cmd)        # Heres where the subprocess should start
-            # self.set_statusbar(size, i)            # Will for the interactive UI
-            continue
+    def run(self, pathlist, outdirectory, format, resolution, eraselist, prefix):     
+        # These variables need to be referenced across multiple file conversions, so I just store it on the 'Mainframe' object
+        self.size = len(pathlist)
+        self.outdirectory = outdirectory
+        self.format = format
+        self.resolution = resolution
+        self.eraselist = eraselist
+        self.prefix = prefix
+        self.counter = 0
+        
+        # This constructs a pool of workers to help you easily multi-thread UIs
+        self.threadpool = QThreadPool() 
+        
+        # Basic checks to make sure we have at-least 1 path available     
+        self.path_list = pathlist
+        if self.path_list:
+            path = self.path_list.pop()
+        if not path:
+            return
+            
+        file = ffmpeg_converter(path, self.outdirectory, self.format, self.resolution, self.eraselist, self.prefix)
+        worker = Worker(self.initialized_worker, file)
+        worker.signals.finished.connect(self.inc_status_bar) # When the worker sends off a 'finished' signal, the we call the 'inc_status_bar' function
+        self.threadpool.start(worker)
+    
+    # This function lets the main object update the status bar, then it starts the next worker (if one exists).
+    def inc_status_bar(self):
+        self.counter += 1
+        self.set_statusbar(self.size, self.counter)
+        if self.path_list:
+            path = self.path_list.pop()
+            file = ffmpeg_converter(path, self.outdirectory, self.format, self.resolution, self.eraselist, self.prefix)
+            worker = Worker(self.initialized_worker, file) # we setup the worker to use 'initialzed_worker' as a callback function, we also supply a 'file' object to the worker
+            worker.signals.finished.connect(self.inc_status_bar)
+            self.threadpool.start(worker)
+    
+    def initialized_worker(self):
+        print('initialized worker')            
+
+class Worker(QRunnable):
+    def __init__(self, fn, *args, **kwargs):
+        super(Worker, self).__init__()
+        
+        self.fn = fn
+        self.file = args[0] # we pull out the file for later use
+        self.args = args # Save any unnamed args (not used right now, but could hold additional information)
+        self.kwargs = kwargs # Save any named args (not used right now, but could hold additional information)
+        self.signals = WorkerSignals()
+
+    @pyqtSlot()
+    def run(self):
+        cmd = self.file.command_concat()
+        self.file.subprocess_run(cmd)
+        
+        # Example code to simulate tasks
+        # import time
+        # import random
+        # sleep_time = random.randint(1,5)
+        # time.sleep(sleep_time)
+        # print("Thread complete")
+
+        self.signals.finished.emit() # Send the signal that the worker has finished
+
+class WorkerSignals(QObject):
+    finished = pyqtSignal()
 
 class ffmpeg_converter():
     def __init__(self, input, output, format, resolution, toErase, prefix):
@@ -199,21 +277,27 @@ class ffmpeg_converter():
         print("prefix: "+ str(self.outprefix))
         print("Eraselist: "+ str(self.toEraseList))
 
-    def rename(self):
-        if self.toEraseList != []:
-            list_of_strings = self.toEraseList.split(";")
-            filename = os.path.basename(self.inputpath)
-            for string in list_of_strings:
+    def rename(self, eraselist, filename):
+        if eraselist != []:
+            for string in eraselist:
                 if string in filename:
-                    del filename[string]
-            print(filename)
+                    print("old filename: " + filename)
+                    filename = filename.replace(string, '')
+                    print("new filename: " + filename)
+            print("removed: "+string)
+
+
             return filename
 
     def command_concat(self):
         filename = os.path.basename(self.fileinputpath)
+        new_filename = self.rename(self.toEraseList, filename)
         br = ' '
-        command = 'ffmpeg -i' + br + str(self.fileinputpath) + br + '-c:v' + br + str(self.fileformat) + br + str(self.fileoutputpath + filename)
+        command = 'ffmpeg -i' + br + str(self.fileinputpath) + br + '-c:v' + br + str(self.fileformat) + br + str(self.fileoutputpath + new_filename)
+        print(command)
         return command
+
+
 
     def subprocess_run(self, cmd):
         subprocess.Popen(cmd).communicate()
